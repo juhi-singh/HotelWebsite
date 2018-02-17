ac8aef5765cf8eba45ece95745a4ca26399b7c18
Dhruv <dhruv.shah4192@gmail.com>
2018-02-17 19:04:19 +0530

    added booking component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { NgxCarouselModule } from 'ngx-carousel';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
 import 'hammerjs';
+
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+
+import { BookingComponent } from './booking/booking.component';
 
 
 @NgModule({
@@ -14,11 +27,18 @@ import { AboutComponent } from './about/about.component';
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    NgxCarouselModule
+    BrowserAnimationsModule,
+    NgxCarouselModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/booking/booking.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDatepicker } from '@angular/material/datepicker';
+
+@Component({
+  selector: 'app-booking',
+  templateUrl: './booking.component.html',
+  styleUrls: ['./booking.component.css']
+})
+export class BookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
